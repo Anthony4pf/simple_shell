28aba1c9600b124864ae119c0c0838a38abb8d1b
Joseph <josbayo09@gmail.com>
2022-12-18 00:31:28 -0800

    get location and execute

--- exec_command.c
+++ exec_command.c
@@ -11,9 +11,13 @@ void exec_command(char **tokens, char **envp)
 {
 	pid_t child_pid;
 	int status;
+	char *command = NULL, *actual_command = NULL;
 
 	if (tokens != NULL)
 	{
+		command = tokens[0];
+		actual_command = get_location(command);
+
 		child_pid = fork();
 		if (child_pid == -1)
 		{
@@ -21,7 +25,7 @@ void exec_command(char **tokens, char **envp)
 		}
 		else if (child_pid == 0)
 		{
-			if (execve(tokens[0], tokens, envp) == -1)
+			if (execve(actual_command, tokens, envp) == -1)
 			{
 				perror("Error");
 			}
